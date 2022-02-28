9036287528baab86df05926165f07152c735cb43
JOSH <53708344+Josh-Algors@users.noreply.github.com>
2022-02-28 01:10:08 +0100

    Update even.py

--- even.py
+++ even.py
@@ -2,8 +2,8 @@ def evenNum():
     even_lists = []
     for i in range(1001):
         if(i%2==0):
-            even_list += [i]
-    return even_list
+            even_lists += [i]
+    return even_lists
 evenNum()
 
 
